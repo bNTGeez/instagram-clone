291f5b2983151c8c29065940ebf7b86e9bbf8cd7
VishrutMehtaVince <64084862+VishrutMehtaVince@users.noreply.github.com>
2024-07-28 00:58:17 -0400

    commit
    Added assets and modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React, { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+import UserContext from "./context/user";
 import * as ROUTES from "./constants/routes.jsx";
+import useAuthListener from "./hooks/use-auth-listener";
 
 const Dashboard = lazy(() => import("./pages/dashboard.jsx"));
 const Login = lazy(() => import("./pages/login.jsx"));
@@ -10,18 +12,21 @@ const Profile = lazy(() => import("./pages/profile.jsx"));
 const NotFound = lazy(() => import("./pages/not-found.jsx"));
 
 function App() {
+  const { user } = useAuthListener();
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-          <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
-          <Route path={ROUTES.PROFILE} element={<Profile />} />
-          <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
-          <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+            <Route path={ROUTES.PROFILE} element={<Profile />} />
+            <Route path={ROUTES.NOT_FOUND} element={<NotFound />} />
+            <Route path={ROUTES.DASHBOARD} element={<Dashboard />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
 
